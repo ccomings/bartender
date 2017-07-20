f884c81eaaaed8d8d89cfcaf4d9038d1cd2c9c75
ccomings <ccomings@gmail.com>
2017-07-20 10:02:46 -0700

    create beers controller

--- app/controllers/api/beers_controller.rb
+++ app/controllers/api/beers_controller.rb
@@ -1,2 +1,49 @@
 class Api::BeersController < ApplicationController
+  before_action :require_logged_in
+
+  def index
+    @beers = Beer.all
+    render :index
+  end
+
+  def create
+    @beer = Beer.new(beer_params)
+
+    if @beer.save
+      render :show
+    else
+      render json: @beer.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @beer = Beer.find(params[:id])
+  end
+
+  def update
+    @beer = Beer.find(params[:id])
+
+    if @beer.update(beer_params)
+      render :show
+    else
+      render json: @beer.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @beer = Beer.find(params[:id])
+
+    if @beer.destroy
+      render :show
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def beer_params
+    params.require(:beer).permit(
+      :name, :description, :abv, :ibu, :image_url
+    )
+  end
 end
