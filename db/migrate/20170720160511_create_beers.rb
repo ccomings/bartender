8cf6927344f9047f29748392036ed6e7cbe300c6
ccomings <ccomings@gmail.com>
2017-07-20 09:29:44 -0700

    create beers table

---
+++ db/migrate/20170720160511_create_beers.rb
@@ -0,0 +1,14 @@
+class CreateBeers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :beers do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+      t.integer :brewery_id, null: false
+      t.string :abv
+      t.string :ibu
+      t.integer :beer_style_id, null: false
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718212311) do
+ActiveRecord::Schema.define(version: 20170720160511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beers", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.integer "brewery_id", null: false
+    t.string "abv"
+    t.string "ibu"
+    t.integer "beer_style_id", null: false
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
